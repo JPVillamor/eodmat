595386cb4b072a033df8cbdaab105422d7c03dfc
JP <jpvillam@mailfence.com>
2022-04-01 21:24:20 -0400

    changed asgi.py

--- eodmat/asgi.py
+++ eodmat/asgi.py
@@ -8,12 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from channels.auth import AuthMiddlewareStack
 from matapp.routing import ws_urlpatterns
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eodmat.settings')
+django.setup()
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
