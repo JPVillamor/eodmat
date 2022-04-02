25f4526ce5ae21119a84358f6bdc110c0901b903
JP <jpvillam@mailfence.com>
2022-04-01 20:09:03 -0400

    added deployment files

--- eodmat/settings.py
+++ eodmat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#o5i0hfva0(oy#m(k$t*fmtouoe2#%31#ch)1t)*1qxdpf8yy%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -127,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "statics"
+    BASE_DIR.joinpath('static')
 ]
 
 # Default primary key field type
