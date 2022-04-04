5dac16da517418d499283af38d758096c5387483
JP Villamor <jpvillam@mailfence.com>
2022-04-04 17:29:43 -0400

    installed apps

--- eodmat/settings.py
+++ eodmat/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'matapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'matapp',
     'channels',
 ]
 
