22267bf482f2db136f567563777e8e0236075fdb
JP Villamor <jpvillam@mailfence.com>
2022-04-04 21:23:00 -0400

    moved statics to static

--- matapp/routing.py
+++ matapp/routing.py
@@ -6,7 +6,7 @@ from channels.auth import AuthMiddlewareStack
 from .consumers import WSConsumer
 
 ws_urlpatterns = [
-    path('ws/some_url/', WSConsumer.as_asgi())
+    path('ws/matserver/', WSConsumer.as_asgi())
 ]
 
 application = ProtocolTypeRouter({
